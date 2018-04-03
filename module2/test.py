8fdace6ef23db1d628f35a0a22c9ff4fea783a2c
jacky <jacky@cngold.com.cn>
2018-04-03 11:28:55 +0800

    update replace.py

--- module2/test.py
+++ module2/test.py
@@ -30,15 +30,16 @@ department:  %s
 # 修改用户信息
 def modify_user_info(account_dic, _user_name):
     user_dic = account_dic[_user_name]
-    for K, V in enumerate(accounts["username"]):
-        print(K, V)
-    choice = input("选择需要修改的内容:").strip()
     while True:
-        if choice.isdigit() and 0 <= int(choice) < len(user_dic):
-            new = input("原内容[%s],输入新内容:" % (user_dic[int(choice)]))
-            user_dic[int(choice)] = new
+        for K, V in enumerate(accounts["username"]):
+            print(K, V)
+        ch = input("选择需要修改的内容:").strip()
+        if ch.isdigit() and 0 <= int(ch) < len(user_dic):
+            new = input("原内容[%s],输入新内容:" % (user_dic[int(ch)]))
+            user_dic[int(ch)] = new
             accounts[_user_name] = user_dic
             write_txt(accounts)
+            print("已修改用户[%s]的%s为%s" % (_user_name, accounts["username"][int(ch)], new))
             break
         else:
             print("输入有误,请选择编号！")
@@ -48,10 +49,7 @@ _user_name = input("输入要查找用户名：").strip()
 if _user_name in accounts.keys():
     print_user_info(accounts, _user_name)
     choice = input("是否要修改用户信息（y确认，任意键退出）：")
-    if choice == "y":
-        modify_user_info(accounts, _user_name)
-    else:
-        exit("bye!")
+    modify_user_info(accounts, _user_name) if choice == "y" else exit("bye!")
 else:
     print("无此用户！bye！")
 
