b961115d67131b8a43f215446d8e0af71af096c0
jacky <jacky@cngold.com.cn>
2018-04-18 11:15:46 +0800

    Merge branch 'master' of github.com:fanhoukui/study
    # Conflicts:
    #	module2/users.txt

--- module2/atm/bank/b_pay.py
+++ module2/atm/bank/b_pay.py
@@ -3,6 +3,8 @@
 import os
 import hashlib
 
+user_dir = "../bank/userdata"  # 由于被mall/m_cart调用这里路径配置为
+
 
 def login(func):
     def wrapper(*args, **kwargs):
@@ -12,8 +14,8 @@ def login(func):
         _password_md5.update(_password.encode(encoding="utf-8"))
         # 导入用户文件成为列表
         account = {}
-        if os.path.exists("../bank/userdata/%s.log" % _username):
-            with open("../bank/userdata/%s.log" % _username, "r", encoding="utf-8") as f:
+        if os.path.exists("%s/%s.log" % (user_dir, _username)):
+            with open("%s/%s.log" % (user_dir, _username), "r", encoding="utf-8") as f:
                 account = eval(f.readline())
             if account["password"] == _password_md5.hexdigest():
                 print("登陆成功,余额%s" % account["balance"])
@@ -27,14 +29,14 @@ def login(func):
 
 @login
 def pay(_username, price):  # 传入用户账户和消费金额扣款
-    with open("../bank/userdata/%s.log" % _username, "r", encoding="utf-8") as f:
+    with open("%s/%s.log" % (user_dir, _username), "r", encoding="utf-8") as f:
         account = eval(f.readline())
     if price > account["balance"]:
         print("余额不足！")
         return False
     else:
         account["balance"] -= price
-        with open("../bank/userdata/%s.log" % _username, 'w', encoding="utf-8") as f:
+        with open("%s/%s.log" % (user_dir, _username), 'w', encoding="utf-8") as f:
             f.write(str(account))
         print("消费成功")
         return True
