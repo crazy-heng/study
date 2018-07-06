5ad1f43f95ae136332d38bfdf69088e3caf62834
jacky <jacky@cngold.com.cn>
2018-07-06 15:23:27 +0800

    Merge branch 'master' of github.com:fanhoukui/study

--- module4/practice.py
+++ module4/practice.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from concurrent.futures import ThreadPoolExecutor
-from threading import Thread, Semaphore, Event
-import time
-import re
-import hashlib
+# from concurrent.futures import ThreadPoolExecutor
+# from threading import Thread, Semaphore, Event
+# import time
+# import re
+# import hashlib
+#
+# sm = Semaphore(10)
+# event = Event()
+# pool = ThreadPoolExecutor(5)
+#
+#
+# def task():
+#     # with sm:
+#         print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+#         time.sleep(2)
 
-sm = Semaphore(10)
-event = Event()
-pool = ThreadPoolExecutor(5)
 
-
-def task():
-    # with sm:
-        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
-        time.sleep(2)
-
-
-def password():
-    for i in range(3):
-        pwd = input('请输入密码:').strip()
-        if len(pwd) < 6:
-            print("错误密码长度小于6位！")
-        elif re.match('^[A-Z]', pwd) == None:
-            print('密码必须大写字母开头！')
-        else:
-            print("yes")
-            m = hashlib.md5()
-            m.update(pwd.encode(encoding="utf-8"))
-            with open('password.txt', 'a') as f:
-                f.write(m.hexdigest() + '\n')
-            return
-    print('超过三次错误！')
+# def password():
+#     for i in range(3):
+#         pwd = input('请输入密码:').strip()
+#         if len(pwd) < 6:
+#             print("错误密码长度小于6位！")
+#         elif re.match('^[A-Z]', pwd) == None:
+#             print('密码必须大写字母开头！')
+#         else:
+#             print("yes")
+#             m = hashlib.md5()
+#             m.update(pwd.encode(encoding="utf-8"))
+#             with open('password.txt', 'a') as f:
+#                 f.write(m.hexdigest() + '\n')
+#             return
+#     print('超过三次错误！')
 
 # if __name__ == '__main__':
 #     print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
@@ -41,7 +41,15 @@ def password():
 #         t.start()
 
 
-if __name__ == '__main__':
-    password()
-    for i in range(10):
-        pool.submit(task)
+# if __name__ == '__main__':
+#     password()
+#     for i in range(10):
+#         pool.submit(task)
+
+import pymysql
+
+conn = pymysql.connect(host='0.0.0.0', user='****', password='***', database='homework')
+cursor = conn.cursor()
+sql = 'INSERT INTO student VALUES(8, "wang", "male", 5)'
+res = cursor.execute(sql)
+print(res)
